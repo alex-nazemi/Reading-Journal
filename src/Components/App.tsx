3c656ed8cc164d9fe8d1d193fc10835d2af2d0aa
alex-nazemi <alexandernazemi@gmail.com>
2023-09-29 00:31:29 +0100

    added backend folders, set up postgres database, created required tables

---
+++ src/Components/App.tsx
@@ -0,0 +1,9 @@
+const App : React.FC = () => { // specifies that App is a functional component
+    return (
+        <>
+            <p>Hello world!</p>
+        </>
+    )
+}
+
+export default App;
